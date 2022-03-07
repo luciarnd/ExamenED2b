7cd705c70f96cd9994dcb545239ad3978a46dcef
LuciaArnedo <daw02.2022@gmail.com>
2022-03-07 12:44:35 +0100

    Proyecto documentado

--- src/examen/Hora.java
+++ src/examen/Hora.java
@@ -1,8 +1,28 @@
 package examen;
 
-public class Hora {
-	private static boolean horaValida;
+/**
+ * Clase que establece si una hora introducida es valida o no
+ * 
+ * @author DAW102
+ * @version 1.8
+ *
+ *
+ */
 
+public class Hora {
+	
+	/**
+	 * Indica si la hora a introducir es valida o no
+	 */
+	public static boolean horaValida;
+	
+	/**
+	 * 
+	 * @param segundos indica los segundos a introducir
+	 * @param minutos indica los minutos a introducir
+	 * @param horas indica las horas a introducir
+	 * @return horaValida establecera si la hora introducida es valida(true) o no(false)
+	 */
 	public static boolean validarHora(int segundos, int minutos, int horas) {
 		horaValida = true;
 		if ((horas >= 0) && (minutos >= 0) && (segundos >= 0)) {
